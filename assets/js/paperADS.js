f9c879c7d5faf2f389ea2cbd0e8053195f89bd1e
sherry_yijun <1054988526@qq.com>
2022-07-16 19:05:34 -0400

    Issue #87 Research and ADS Track Final Schedules
    Two pages were added. Please let me know if there's anything I can change.

---
+++ assets/js/paperADS.js
@@ -0,0 +1,26 @@
+//import data
+import adsData from '/csv/ads.json' assert {type: 'json'};
+
+//matching patterns
+const regexSession = new RegExp('^SESSION');
+
+
+//variables
+let ADSHTML = "";
+
+
+
+for(var i = 0; i<adsData.length; i++){
+	if(regexSession.test(adsData[i].r1)){
+		ADSHTML += `<p class="session">` + adsData[i].r1 + `</p>`;
+	}
+	else if(adsData[i].r1.includes("August") || adsData[i].r1.includes("All Other Accepted Papers")){
+		ADSHTML +=  `<p class="title">` + adsData[i].r1 + `</p>`;
+	}
+	else if(!!adsData[i].r1 && !!adsData[i].r2){
+		ADSHTML += `<section class="paper"><p class="paperTitle">` + adsData[i].r1 + `</p><p class="paperAuthor">` + adsData[i].r2 + `</p></section>`;
+	}
+}
+
+$(".papers").html(ADSHTML);
+
---
+++ assets/js/paperRT.js
@@ -0,0 +1,34 @@
+//import data
+
+import researchData from '/csv/research.json' assert {type: 'json'};
+
+//matching patterns
+const regexSession = new RegExp('^SESSION');
+
+//variables
+let researchHTML = "";
+
+
+
+for(var i = 0; i<researchData.length; i++){
+	if(regexSession.test(researchData[i].r1)){
+		researchHTML += `<p class="session">` + researchData[i].r1 + `</p>`;
+	}
+	else if(researchData[i].r1.includes("August") || researchData[i].r1.includes("All Other Accepted Papers")){
+		researchHTML +=  `<p class="title">` + researchData[i].r1 + `</p>`;
+	}
+	else if(!!researchData[i].r1 && !!researchData[i].r2){
+		researchHTML += `<section class="paper"><p class="paperTitle">` + researchData[i].r1 + `</p><p class="paperAuthor">` + researchData[i].r2 + `</p></section>`;
+	}
+}
+
+$(".papers").html(researchHTML);
+
+
+
+
+
+
+
+
+
