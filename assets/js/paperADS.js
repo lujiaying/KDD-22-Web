e0d1d5f09824c69fe08c307808853b2492e41c0a
sherry_yijun <1054988526@qq.com>
2022-07-19 12:59:55 -0400

    fixed bug in #100 and updated the paper lists

--- assets/js/paperADS.js
+++ assets/js/paperADS.js
@@ -1,26 +1,31 @@
-//import data
-import adsData from '../csv/ads.json' assert {type: 'json'};
+//variables
+let ADSHTML = "";
+let adsData = [];
 
 //matching patterns
 const regexSession = new RegExp('^SESSION');
 
 
-//variables
-let ADSHTML = "";
-
-
-
-for(var i = 0; i<adsData.length; i++){
-	if(regexSession.test(adsData[i].r1)){
-		ADSHTML += `<p class="session">` + adsData[i].r1 + `</p>`;
-	}
-	else if(adsData[i].r1.includes("August") || adsData[i].r1.includes("All Other Accepted Papers")){
-		ADSHTML +=  `<p class="title">` + adsData[i].r1 + `</p>`;
-	}
-	else if(!!adsData[i].r1 && !!adsData[i].r2){
-		ADSHTML += `<section class="paper"><p class="paperTitle">` + adsData[i].r1 + `</p><p class="paperAuthor">` + adsData[i].r2 + `</p></section>`;
-	}
+window.onload = function(){
+	fetch('/csv/ads.json')
+	.then(function(resp){
+		return resp.json();
+	})
+	.then(function(data){
+		adsData = data;
+		for(var i = 0; i<adsData.length; i++){
+			if(regexSession.test(adsData[i].r1) || adsData[i].r1.includes("ADS PAPER SHOWCASE")){
+				ADSHTML += `<p class="session">` + adsData[i].r1 + `</p>`;
+			}
+			else if(adsData[i].r1.includes("August") || adsData[i].r1.includes("All Other Accepted Papers")){
+				ADSHTML +=  `<p class="title">` + adsData[i].r1 + `</p>`;
+			}
+			else if(!!adsData[i].r1 && !!adsData[i].r2){
+				ADSHTML += `<section class="paper"><p class="paperTitle">` + adsData[i].r1 + `</p><p class="paperAuthor">` + adsData[i].r2 + `</p></section>`;
+			}
+		}
+		
+		$(".papers").html(ADSHTML);
+	})
 }
-
-$(".papers").html(ADSHTML);
 
--- assets/js/paperRT.js
+++ assets/js/paperRT.js
@@ -1,28 +1,40 @@
-//import data
-
-import researchData from '../csv/research.json' assert {type: 'json'};
-
 //matching patterns
 const regexSession = new RegExp('^SESSION');
 
 //variables
 let researchHTML = "";
+let researchData = [];
 
-
-
-for(var i = 0; i<researchData.length; i++){
-	if(regexSession.test(researchData[i].r1)){
-		researchHTML += `<p class="session">` + researchData[i].r1 + `</p>`;
-	}
-	else if(researchData[i].r1.includes("August") || researchData[i].r1.includes("All Other Accepted Papers")){
-		researchHTML +=  `<p class="title">` + researchData[i].r1 + `</p>`;
-	}
-	else if(!!researchData[i].r1 && !!researchData[i].r2){
-		researchHTML += `<section class="paper"><p class="paperTitle">` + researchData[i].r1 + `</p><p class="paperAuthor">` + researchData[i].r2 + `</p></section>`;
-	}
+window.onload = function(){
+	fetch('/csv/research.json')
+	.then(function(resp){
+		return resp.json();
+	})
+	.then(function(data){
+		researchData = data;
+		for(var i = 0; i<researchData.length; i++){
+			if(regexSession.test(researchData[i].r1)){
+				researchHTML += `<p class="session">` + researchData[i].r1 + `</p>`;
+			}
+			else if(researchData[i].r1.includes("August") || researchData[i].r1.includes("All Other")){
+				researchHTML +=  `<p class="title">` + researchData[i].r1 + `</p>`;
+			}
+			else if(!!researchData[i].r1 && !!researchData[i].r2){
+				researchHTML += `<section class="paper"><p class="paperTitle">` + researchData[i].r1 + `</p><p class="paperAuthor">` + researchData[i].r2 + `</p></section>`;
+			}
+		}
+		
+		$(".papers").html(researchHTML);
+	})
 }
 
-$(".papers").html(researchHTML);
+
+
+
+
+
+
+
 
 
 
